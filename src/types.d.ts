f40399a19265a27006dd9296c2d834b67a5653a9
Lete114 <me@imlete.cn>
2025-07-15 10:48:54 +0800

    feat: language variants are no longer supported

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,7 @@
 // https://developers.deepl.com/docs/getting-started/supported-languages
-export type TVariant = 'EN-GB' | 'EN-US' | 'PT-BR' | 'PT-PT' | 'ZH-HANS' | 'ZH-HANT'
 export type TLanguage = 'AR' | 'BG' | 'CS' | 'DA' | 'DE' | 'EL' | 'EN' | 'ES' | 'ET' | 'FI' | 'FR' | 'HU' | 'ID' | 'IT' | 'JA' | 'KO' | 'LT' | 'LV' | 'NB' | 'NL' | 'PL' | 'PT' | 'RO' | 'RU' | 'SK' | 'SL' | 'SV' | 'TR' | 'UK' | 'ZH'
 export type TSourceLanguage = 'AUTO' | TLanguage
-export type TTargetLanguage = TLanguage | TVariant
+export type TTargetLanguage = TLanguage
 
 export interface IOptions {
   from: TSourceLanguage
